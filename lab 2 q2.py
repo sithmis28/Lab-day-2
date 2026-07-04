aa7ffcf4a0df99fea73e8a35f62e48f6a430181d
Sithmi Sanjana <saachis30@gmail.com>
2026-07-04 10:38:52 +0530

    Add files via upload

---
+++ lab 2 q2.py
@@ -0,0 +1,11 @@
+#first aproach
+
+print("22 devide by 7 is :3.14285...")
+
+# 2nd aproach
+print("22 devide by 7 is :",22/7)
+
+#3rd
+result=22 
+print("22 devide by 7 is :", result)
+print("value pi is :",result) 
